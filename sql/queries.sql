fd1365002140711058104228f08769529e0ecf56
Ma. Gabriellee <https://github.com/whz-gabrielle/IT105-Midterm-Sapico-Ma.Gabrielle>
2026-04-02 16:37:39 +0800

    Added SQL queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,10 +11,21 @@ VALUES ('The Silent Patient', '1212121212', 2019, 'Thriller', 4);
 INSERT INTO Authors (author_name)
 VALUES ('Alex Michaelides');
 
--- Link book to author
-INSERT INTO Book_Authors (book_id, author_id)
-VALUES (LAST_INSERT_ID(), LAST_INSERT_ID());  -- assumes last inserted book/author
-
 UPDATE Books
 SET copies_available = copies_available - 1
 WHERE book_id = 3;
+
+UPDATE Members
+SET phone = '09381234567'
+WHERE member_id = 2;
+
+DELETE FROM Books
+WHERE book_id = 10;
+
+SELECT m.first_name, m.last_name, b.title, br.borrow_date, br.due_date
+FROM Members m
+JOIN Borrowing br ON m.member_id = br.member_id
+JOIN Borrow_Details bd ON br.borrow_id = bd.borrow_id
+JOIN Books b ON bd.book_id = b.book_id
+WHERE m.member_id = 1;
+
